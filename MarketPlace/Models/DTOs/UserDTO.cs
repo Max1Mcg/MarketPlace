5f85587d733edf40fea7ed8bb6d8354d608899a1
Max1M <leggo2001@mail.ru>
2023-07-16 20:07:07 +0300

    Добавил дто для добавления пользователя

---
+++ MarketPlace/Models/DTOs/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace MarketPlace.Models.DTOs
+{
+    public class UserDTO
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Patronymic { get; set; }
+        public int? Age { get; set; }
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+    }
+}
