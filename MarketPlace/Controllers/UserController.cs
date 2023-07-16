3e7e4918809569c128ac1b09c7e58fba618aa345
Max1M <leggo2001@mail.ru>
2023-07-16 20:05:47 +0300

    Добавил контроллер для пользователя с добавлением и получением

---
+++ MarketPlace/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MarketPlace.Models.DTOs;
+using MarketPlace.Services.Interfaces;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MarketPlace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        // GET: api/<ValuesController>
+        [HttpPost("/registration")]
+        public async Task<IActionResult> Registration([FromBody]UserDTO userDTO)
+        {
+            try
+            {
+                return Ok(await _userService.Registration(userDTO));
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPost("/user")]
+        public ActionResult User(Guid id)
+        {
+            try
+            {
+                return Ok(_userService.GetUser(id));
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
