c5636e80ea42f0a72e949aca1b922a7ca37867af
Max1M <leggo2001@mail.ru>
2023-07-24 13:52:57 +0300

    Добавил перечисление для статусов

---
+++ MarketPlace/Enums/StatusEnum.cs
@@ -0,0 +1,22 @@
+﻿namespace MarketPlace.Enums
+{
+    enum StatusEnum
+    {
+        /// <summary>
+        /// Сформирован
+        /// </summary>
+        Formed = 0,
+        /// <summary>
+        /// Оплачен
+        /// </summary>
+        Paid,
+        /// <summary>
+        /// Доставлен
+        /// </summary>
+        Delivered,
+        /// <summary>
+        /// Отменен
+        /// </summary>
+        Closed
+    }
+}
