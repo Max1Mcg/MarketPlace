4835b275f15cc8887ec91d0fb60fb430c7e7c680
Max1M <leggo2001@mail.ru>
2023-07-24 13:51:53 +0300

    Добавил новые дтошки

--- MarketPlace/Models/DTOs/ItemDTO.cs
+++ MarketPlace/Models/DTOs/ItemDTO.cs
@@ -2,6 +2,7 @@
 {
     public class ItemDTO
     {
+        public Guid Userid { get; set; }
         public double? Cost { get; set; }
         public string? Description { get; set; }
         public string? Weight { get; set; }
